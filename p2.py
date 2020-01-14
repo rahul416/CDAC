4ed053d6b2b6ea0fca3f1b64a1a763ab4dea6e83
rahul416 <59193388+rahul416@users.noreply.github.com>
2020-01-14 10:02:06 +0530

    Update p2.py

--- p2.py
+++ p2.py
@@ -1,5 +1,5 @@
 num1 = 1
 num2 = 6
-
-sum = num1 + num2 
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+num3 = 1
+sum = num1 + num2 + num3 
+print('The sum of {0} , {1} and {2} is {3}'.format(num1, num2, num3, sum))
